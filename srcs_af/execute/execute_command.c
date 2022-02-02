53110befb53a1ff3835293aa440973281ebad698
Alexandro Fulmini <alexandrofulmini@Alexandros-MBP.home>
2022-02-02 12:56:55 +0100

    execute/path check needs to be finalised

--- srcs_af/execute/execute_command.c
+++ srcs_af/execute/execute_command.c
@@ -6,7 +6,7 @@
 /*   By: afulmini <afulmini@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 16:54:38 by afulmini          #+#    #+#             */
-/*   Updated: 2022/02/02 11:58:57 by afulmini         ###   ########.fr       */
+/*   Updated: 2022/02/02 12:56:25 by afulmini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,10 +49,10 @@ void	execute_command(t_shell *shell, t_cmd *cmd)
 		return ;
 	program = ft_to_lower(cmd->args[0]);
 	if (get_builtin(program) != NULL)	// ==> check if the program is a builtin function we built
-		get_builtin(program)(shell, cmd->args);	// check this type of function prototype
+		get_builtin(program)(shell, cmd->args);	// this prototype allows to check a condition before acting upon said condition
 	else if (ft_contains_char(program, '/'))
 	{
-		if (check_if_exist(NULL, program))
+		if (check_if_exist(NULL, program))	// check if path exist in env $PATH of our shell
 			execute_program();	//define function in execute_program.c ==>
 			// we need the shell the path and the cmd itself
 		else
@@ -61,7 +61,7 @@ void	execute_command(t_shell *shell, t_cmd *cmd)
 	else
 	{
 		program_path = get_program_path(shell, cmd->args[0]);
-		execute_program();
+		execute_program();	// define function
 		free(program_path);
 	}
 }
