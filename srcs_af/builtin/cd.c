56eafb68dab82b8b1fcc3861573d6e8f26a3355a
Alexandro Fulmini <alexandrofulmini@Alexandros-MBP.home>
2022-02-03 17:57:39 +0100

    commenting and checking parser + start to work on process + exec

--- srcs_af/builtin/cd.c
+++ srcs_af/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: afulmini <afulmini@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 12:15:21 by afulmini          #+#    #+#             */
-/*   Updated: 2022/01/31 18:37:25 by afulmini         ###   ########.fr       */
+/*   Updated: 2022/02/03 17:57:27 by afulmini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	cd(t_shell *shell, char **cmd)
 {
 	char	*path;
 	char	*old_pwd;
+	char	*new_pwd;
 
 	if (cmd[1] == NULL || ft_strcmp(cmd[1], "~") == 0)
 		path = get_env_var(shell, "HOME");
@@ -23,6 +24,7 @@ void	cd(t_shell *shell, char **cmd)
 		path = ft_strdup(cmd[1]);
 	if (path == NULL)
 		return ;
+	new_pwd = NULL;
 	old_pwd = getcwd(NULL, 0);
 	if (chdir(path) == -1)
 	{
@@ -31,10 +33,13 @@ void	cd(t_shell *shell, char **cmd)
 	}
 	else
 	{
+		new_pwd = getcwd(NULL, 0);
 		set_env(shell, "OLDPWD", old_pwd, FALSE);
+		set_env(shell, "PWD", new_pwd, FALSE);
 		update_prompt(shell);
 		shell->exit_status = 0;
 	}
 	free(path);
 	free(old_pwd);
+	free(new_pwd);
 }
