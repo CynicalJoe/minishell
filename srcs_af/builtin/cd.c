7ddb1774d2e1b5cbcec5b9c1f72122cf55fcdb5c
Alexandro Fulmini <alexandrofulmini@Alexandros-MBP.home>
2022-02-05 18:04:26 +0100

    builtin 70% done

--- srcs_af/builtin/cd.c
+++ srcs_af/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: afulmini <afulmini@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 12:15:21 by afulmini          #+#    #+#             */
-/*   Updated: 2022/02/04 08:46:46 by afulmini         ###   ########.fr       */
+/*   Updated: 2022/02/05 18:02:35 by afulmini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,12 @@ void	update_pwd(t_shell *shell, char *pwd)
 
 	new_pwd = NULL;
 	old_pwd = pwd;
-	// update the envs of our shell
 	new_pwd = getcwd(NULL, 0);
 	set_env(shell, "OLDPWD", old_pwd, FALSE);
 	set_env(shell, "PWD", new_pwd, FALSE);
 	update_prompt(shell);
 	shell->exit_status = 0;
 }
-
 
 void	my_cd(t_shell *shell, char **cmd)
 {
@@ -44,60 +42,14 @@ void	my_cd(t_shell *shell, char **cmd)
 	old_pwd = getcwd(NULL, 0);
 	if (chdir(path) == -1)
 	{
-		// path does not exist
-		// print error ==> ("cd", path, strerror(errno));
+		put_error("cd", path, strerror(errno));
 		shell->exit_status = 1;
 	}
 	else
 	{
-		// update the env of our shell
 		update_pwd(shell, old_pwd);
 	}
 	free(path);
 	free(old_pwd);
 	free(new_pwd);
 }
-
-
-/* 
-void	update_pwd(t_shell *shell, char *pwd)
-{
-	char	*old_pwd;
-	char	*new_pwd;
-
-	new_pwd = NULL;
-	old_pwd = pwd;
-	// update the envs of our shell
-	new_pwd = getcwd(NULL, 0);
-	set_env_var(shell, "OLDPWD", old_pwd, FALSE);
-	set_env_var(shell, "PWD", new_pwd, FALSE);
-	update_prompt(shell);
-	shell->exit_status = 0;
-}
-
-
-void	cd_builtin(t_shell *shell, char **cmd)
-{
-	char	*path;
-	char	*old_pwd;
-	char	*new_pwd = NULL;	//mine
-
-	if (cmd[1] == NULL || ft_strcmp(cmd[1], "~") == 0)
-		path = get_env_var(shell, "HOME");
-	else
-		path = ft_strdup(cmd[1]);
-	if (path == NULL)
-		return ;
-	old_pwd = getcwd(NULL, 0);
-	if (chdir(path) == -1)
-	{
-		put_error("cd", path, strerror(errno));
-		shell->exit_status = 1;
-	}
-	else
-		update_pwd(shell, old_pwd);
-	free(path);
-	free(old_pwd);
-	free(new_pwd);		//mine
-}
-*/
--- srcs_af/builtin/export.c
+++ srcs_af/builtin/export.c
@@ -6,76 +6,9 @@
 /*   By: afulmini <afulmini@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 18:49:47 by afulmini          #+#    #+#             */
-/*   Updated: 2022/02/04 16:42:41 by afulmini         ###   ########.fr       */
+/*   Updated: 2022/02/05 18:04:12 by afulmini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-static char	*get_export_key(char *var)
-{
-	char	*key;
-	size_t	i;
-
-	key = NULL;
-	i = 0;
-	while (var[i] && var[i] != '=')
-	{
-		key = ft_append_char_to_str(key, var[i]);
-		i++;
-	}
-	return (key);
-}
-
-static void	export_var(t_shell *shell, char *var)
-{
-	ssize_t	equal_index;
-	char	*key;
-
-	equal_index = ft_find_char(var, '=');		// define function in libft
-	key = get_export_key(var);
-	if (equal_index == -1)
-		set_env(shell, var, NULL, FALSE);
-	else
-		set_env(shell, key, &var[equal_index + 1], FALSE);
-	free(key);
-	shell->exit_status = 0;
-}
-
-static void	display_env_var(t_shell *shell)
-{
-	char	**sorted_env;
-	size_t	i;
-
-	sorted_env = ft_sort_str_array(shell->env);			//define function in libft
-	i = 0;
-	while (sorted_env[i] != NULL)
-	{
-		ft_putstr_fd("declare -x", 1);
-		ft_putendl_fd(sorted_env[i], 1);
-		i++;
-	}
-	ft_destroy_strarray(&sorted_env);
-	shell->exit_status = 0;
-}
-
-void	my_export(t_shell *shell, char **cmd)
-{
-	size_t	i;
-
-	i = 1;
-	if (cmd[i] == NULL)
-		display_env_var(shell);
-	while (cmd[i] != NULL)
-	{
-		if (is_alpha(cmd[i][0]) || cmd[i][0] == '-')
-			export_var(shell, cmd[i]);
-		else
-		{
-			put_error("minishell", cmd[i], "not a valid identifier.");
-			shell->exit_status = 1;
-			return ;
-		}
-		i++;
-	}
-}
