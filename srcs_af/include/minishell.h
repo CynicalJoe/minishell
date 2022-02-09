c10d60544e5440322322e1e61357f2ebc7fec900
Alexandro Fulmini <alexandrofulmini@Alexandros-MBP.home>
2022-02-09 08:41:32 +0100

    check path

--- srcs_af/include/minishell.h
+++ srcs_af/include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: afulmini <afulmini@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 11:28:59 by afulmini          #+#    #+#             */
-/*   Updated: 2022/02/08 11:58:06 by afulmini         ###   ########.fr       */
+/*   Updated: 2022/02/08 17:25:08 by afulmini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@
 # define TRUE 1
 # define SUCCESS 0
 # define FALSE 0
+
 
 # include <unistd.h>
 # include <string.h>
@@ -58,7 +59,7 @@ typedef struct s_cmd
 	struct s_cmd	*next;			// link between the different cmds structs
 	// piped flag defines the link to the next command
 	int				piped;			// use int? or bool? TRUE or FALSE or 0 1 2
-	int				in_out;			// use for redirection
+	// int				in_out;			// use for redirection
 	pid_t			pid;
 	int				pipe[2];
 }	t_cmd;
@@ -82,7 +83,7 @@ typedef struct s_shell
 	char			*prompt;		// prompt for user to input commands
 	t_cmd_container	cmd_container;	// connect to container struct
 	int				exit_status;	// exit status --> make it a global variable?
-	int				in_exec;		// use int instead of bool thus TRUE == 1 & FALSE == 0;
+	bool			in_exec;		// use int instead of bool thus TRUE == 1 & FALSE == 0;
 	int				level;			// update the shell level we are in --> each call of minishell += 1 level
 }	t_shell;
 
@@ -97,7 +98,7 @@ int	main(int ac, char **av, char **env);
 
 bool	put_error(char *program, char *part1, char *part2);
 
-// signals
+// 			src/signals
 // check for signals interpretation depending on the level and exec you are currently in
 void	ctrl_backslash_signal(int sig);
 void	ctrl_c_signal(int sig);
@@ -110,7 +111,7 @@ void	check_and_give_token(t_cmd_container *cmd_container, char c);
 void	tokenise_pipe(t_cmd_container *cmd_container);
 void	tokenise_quote(t_cmd_container *cmd_container, char quote);
 void	tokenise_redir(t_cmd_container *cmd_container, char redir);
-int		tokenise(t_cmd_container *cmd_container);
+bool	tokenise(t_cmd_container *cmd_container);
 
 size_t	get_cmd_size(t_cmd_container *cmd_container);
 char	**ft_append_str_to_str_array(char **strarray, char *str, bool free_array);
@@ -123,6 +124,7 @@ char	**create_env(char **default_env);
 void	*destroy_shell(t_shell *shell);
 t_shell create_shell(char **env);
 void	update_prompt(t_shell *shell);
+void	exit_shell(t_shell *shell, char*message, int exit_code);
 
 // get and set env variables
 size_t	get_env_len(t_shell *shell);
@@ -141,7 +143,7 @@ void	*destroy_cmd(t_cmd *cmd);
 size_t	get_cmd_size(t_cmd_container *cmd_container);		// get the size of the commands in the container
 void	next_cmd(t_cmd_container *cmd_container);			// gets next command in the container struct
 
-//			src/process/ 		==> process commands before executing them
+//			src/execute/ 		==> process commands before executing them
 
 char	*processed_args(t_shell *shell, char *arg);
 char	*get_processed_quote(t_shell *shell, char *args, size_t len, size_t *i);
@@ -155,7 +157,7 @@ char	*get_program_path(t_shell *shell, char *program);
 bool	check_if_exist(char *path, char *program);
 char	*build_path(char *path, char *program);
 
-// exec program
+//			 exec program
 void	execute_command(t_shell *shell, t_cmd *cmd);
 void	execute_program(t_shell *shell, char *path, t_cmd *cmd);
 
@@ -173,5 +175,13 @@ void	my_export(t_shell *shell, char **cmd);
 // utils
 int	ft_contains_char(char *str, char c);
 ssize_t	ft_find_char(char *s, char c);
+
+
+
+// TESTING
+void	display_env_var(t_shell *shell);
+void	display_cmd(t_cmd *cmd);
+void	display_cmd_container(t_cmd_container *cmd_container);
+
 
 #endif
