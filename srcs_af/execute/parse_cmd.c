46b2c220eab7c9488b89cf26bab1edd6ab9594f3
Alexandro Fulmini <alexandrofulmini@Alexandros-MacBook-Pro.local>
2022-02-09 13:09:36 +0100

    clean parse

--- srcs_af/execute/parse_cmd.c
+++ srcs_af/execute/parse_cmd.c
@@ -6,11 +6,16 @@
 /*   By: afulmini <afulmini@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 16:25:24 by afulmini          #+#    #+#             */
-/*   Updated: 2022/02/04 16:44:47 by afulmini         ###   ########.fr       */
+/*   Updated: 2022/02/09 12:47:46 by afulmini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+char	*get_quotes_process()
+{
+	
+}
 
 bool	is_redir(char *arg)
 {
@@ -20,6 +25,30 @@ bool	is_redir(char *arg)
 		|| ft_strcmp(arg, "<") == 0)
 		return (TRUE);
 	return (FALSE);
+}
+
+char	*processed_args(t_shell *shell, char *arg)
+{
+	char	*processed;
+	size_t	len;
+	size_t	i;
+
+	processed = NULL;
+	i = 0;
+	len = ft_strlen(arg);
+	while (i < len)
+	{
+		if (arg[i] == '\'' || arg[i] == '\"')
+			processed = ft_append_str(processed, get_quotes_process(), TRUE, TRUE);
+		else if (arg[i] == '$' && (ft_isalnum(arg[i + 1]) || arg[i + 1] == '?'))
+			processed = ft_append_env_to_str(shell, processed, get_env_var_name(arg, len, &i));
+		else
+		{
+			processed = ft_append_char_to_str(processed, arg[i]);
+			i++;
+		}
+	}
+	return (processed);
 }
 
 void	parse_cmd(t_shell *shell, t_cmd *cmd)
