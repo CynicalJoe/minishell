d760ff47dffc86ce16c90ba81c1bf1eaea8bba84
Alexandro Fulmini <alexandrofulmini@Alexandros-MBP.home>
2022-02-15 21:44:03 +0100

    no leaks and flags handled -- need to norm

--- srcs_af/execute/process_utils.c
+++ srcs_af/execute/process_utils.c
@@ -6,31 +6,29 @@
 /*   By: afulmini <afulmini@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 13:01:25 by afulmini          #+#    #+#             */
-/*   Updated: 2022/02/05 09:18:45 by afulmini         ###   ########.fr       */
+/*   Updated: 2022/02/15 21:41:52 by afulmini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-// appends 
 char	*ft_append_env_to_str(t_shell *shell, char *str, char *key)
 {
 	char	*res;
 
-	
 	res = ft_append_str(str, get_env_var(shell, key), TRUE, TRUE);
 	free(key);
 	return (res);
 }
 
-// funciton to get the Variable called
 char	*get_env_var_name(char *arg, size_t len, size_t *i)
 {
 	char	*res;
-	
+
 	res = NULL;
 	(*i)++;
-	while (*i < len && (ft_isalnum(arg[*i]) || arg[*i] == '?' || arg[*i] == '_'))	// 
+	while (*i < len && (ft_isalnum(arg[*i])
+			|| arg[*i] == '?' || arg[*i] == '_'))
 	{
 		res = ft_append_char_to_str(res, arg[*i]);
 		(*i)++;
